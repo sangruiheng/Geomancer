667bf959f75d52fb042b6036b01ce76984358bb7
sangruiheng <919915113@qq.com>
2019-04-08 17:07:01 +0800

    first

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -9,7 +9,7 @@ return array(
     //数据库链接
     'DB_TYPE' => 'mysql', // 数据库类型
     'DB_HOST' => '127.0.0.1', // 服务器地址
-    'DB_NAME' => 'books', // 数据库名
+    'DB_NAME' => 'geomancer', // 数据库名
     'DB_USER' => 'root', // 用户名
     'DB_PWD' => 'root', // 密码
     'DB_PORT' => 3306, // 端口
