a2eb005e440c4ef58fd7c265e2ab55809596f8e2
sangruiheng <919915113@qq.com>
2019-04-08 18:41:01 +0800

    1

---
+++ Application/Manage/Controller/GeomancerController.class.php
@@ -0,0 +1,37 @@
+<?php
+namespace Manage\Controller;
+use Think\Controller;
+
+class GeomancerController extends CommonController {
+
+    public function userList(){
+
+
+        if (!empty($_GET['keyWord'])) {
+            $map = $this->Search('user', $_GET['keyWord']);
+        }
+        $p = $_GET['p'];
+        if(empty($p)){
+            $p = 1;
+        }
+        $map['addTime'] = array('neq' ,'');
+        $user = D('user')->where($map)->order('id desc')->page($p.',10')->select();
+        foreach ($user as &$value){
+            $value['nickName'] = urldecode($value['nickName']);
+        }
+//        print_r($user);
+        $count = D('user')->where($map)->count();
+        $Page = getpage($count, 10);
+        foreach($map as $key=>$val) {
+            $page->parameter .= "$key=".urlencode($val).'&';
+        }
+        $this->assign('page', $Page->show());
+        $this->assign('list', $user);
+        $this->display();
+
+
+    }
+    
+
+}
+?>
